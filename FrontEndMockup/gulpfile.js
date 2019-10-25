f4db1baeda3bce62025cb88b4b324543e3aacd44
Mike McDonagh <mikemcdonagh@protonmail.com>
2019-10-24 20:32:43 -0400

    HTML and start of CSS Mockup

---
+++ FrontEndMockup/gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var cleanCSS = require('gulp-clean-css');
+var concat = require('gulp-concat');
+
+gulp.task('sass', function() {
+    return gulp.src('./css/*.scss')
+        .pipe(concat('main.scss'))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('sass:watch', function() {
+    gulp.watch('./css/*.scss', ['sass']);
+});
+
+gulp.task('uglyfy')
+
+gulp.task('default', ['sass', 'sass:watch']);
