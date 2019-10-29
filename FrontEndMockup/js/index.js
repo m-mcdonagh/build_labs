00418c87f53db866611d02452564c34d9fcf3bc4
Mike McDonagh <mikemcdonagh@protonmail.com>
2019-10-29 15:37:50 -0400

    Added color transition to animation at "/"

--- FrontEndMockup/js/index.js
+++ FrontEndMockup/js/index.js
@@ -1,6 +1,6 @@
 $(function() {
     $('.clip-btn').on('click', function() {
-        $('#clip-toggler').attr('checked', true);
+        $($(this).attr('data-toggler')).attr('checked', true);
         $('#blue-ham').fadeOut(500);
         
         $coverimg = $('#cover-img');
@@ -12,12 +12,12 @@ $(function() {
             'left': '10px',
             'top' : '0',
             'height' : '5vh'
-        }, 650);
+        }, 600);
         $coverimg.remove();
 
         $('.clip-btn').remove();
 
-        let url = $(this).attr('data-relocation');
+        let url = $(this).attr('data-url');
         setTimeout(function(){
             window.location = url;
         }, 750);
