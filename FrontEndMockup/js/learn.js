68625bc30b7c3549a374337909d7c48d46c9be0c
Mike McDonagh <mikemcdonagh@protonmail.com>
2019-11-11 11:07:30 -0500

    Transitioned /learn.html to materialize (not /lab.html)

--- FrontEndMockup/js/learn.js
+++ FrontEndMockup/js/learn.js
@@ -1,20 +1,3 @@
 $(function(){
-    placechecks();
-    window.onresize = placechecks;
-
-    $('#lab-col span').on('click', function(){
-        window.location = '/lab.html';
-    })
+    $('.tooltipped').tooltip()
 });
-
-function placechecks() {
-    var left = $('#check-col').width()/2 - $('#check-col').width()/7;
-    var height = $('#check-col span').height();
-    $('.check').each(function(i, elem){
-        $(elem).css({
-            height: height,  
-            left: left,
-            top: $($(elem).attr('data-lab')).position().top - height/4.5
-        });
-    });
-}
