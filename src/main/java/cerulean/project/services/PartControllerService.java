78efa64ec0b0400c95ef1016b54d65b7b819c822
Brian Yi <byi8220@gmail.com>
2019-11-13 22:59:18 -0500

    untested db interfacing code

--- src/main/java/cerulean/project/services/PartControllerService.java
+++ src/main/java/cerulean/project/services/PartControllerService.java
@@ -1,4 +1,39 @@
 package cerulean.project.services;
 
+import cerulean.project.database.AccountRepository;
+import cerulean.project.database.PartRepository;
+import cerulean.project.models.Account;
+import cerulean.project.models.LabAssignment;
+import cerulean.project.models.Part;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class PartControllerService {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Autowired
+    private PartRepository partRepository;
+
+    public void addNewPart(Account creator, Part newPart) {
+
+        creator.getCreatedParts_ids().add(newPart.get_id());
+        accountRepository.save(creator);
+        partRepository.save(newPart);
+    }
+
+    public List<Part> getPartsCreatedByUser(Account user) {
+        List<Part> result = new ArrayList<>();
+        partRepository.findAllById(user.getCreatedParts_ids()).forEach(result::add);
+        return result;
+    }
+
+    public Part getPart(String partId) {
+        return partRepository.findById(partId).orElse(null);
+    }
 }
--- src/main/java/cerulean/project/services/StepControllerService.java
+++ src/main/java/cerulean/project/services/StepControllerService.java
@@ -1,4 +1,13 @@
 package cerulean.project.services;
 
+import cerulean.project.models.LabAssignment;
+import cerulean.project.models.Step;
+import org.springframework.stereotype.Component;
+
+@Component
 public class StepControllerService {
+
+    public boolean isStepValid(Step step, LabAssignment labAssignment) {
+        return true;
+    }
 }
