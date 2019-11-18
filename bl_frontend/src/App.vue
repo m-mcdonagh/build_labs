7825d82e717873fce161bd364cc2f5bb08b04e86
Mike McDonagh <mikemcdonagh@protonmail.com>
2019-11-18 15:38:52 -0500

    Brought materialize, imgs, and navbar to vue

--- bl_frontend/src/App.vue
+++ bl_frontend/src/App.vue
@@ -1,36 +1,101 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/register">About</router-link>
-      <router-link to="/create">About</router-link>
-      <router-link to="/buildLab">About</router-link>
-      <router-link to="/about">About</router-link>
-
-    </div>
+    <nav class="indigo lighten-1">
+        <div class="nav-wrapper">
+            <a id="cover" class="left" href="/"><img class="brand-logo" src="./assets/img/cover.png"></a>
+            <ul id="nav-mobile" class="right">
+                <li><a href="/logout"><i class="material-icons">power_settings_new</i></a></li>
+            </ul>
+        </div>  
+    </nav>
     <router-view/>
   </div>
 </template>
 
+<style>
+  @import './assets/css/materialize.css';
+</style>
+
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
 }
 
-#nav {
-  padding: 30px;
+#app{
+    font-family: "Roboto", sans-serif;
+    color: white;
+    overflow: hidden;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+    nav {
+      position: fixed;
+      height: 5vh;
+      width: 100%;
+      top: 0;
+      z-index: 1;
+    
+      #cover {
+        display: contents;
+        
+        img {
+          position: absolute;
+          left: 10px;
+          height: 100%;
+        }
+      }
     }
-  }
+    
+    .main {
+        display: flex;
+        background-image: linear-gradient(#FFFFFF, #CFCFCF);
+        width: 100vw;
+        height: 95vh;
+        margin: 0;
+        padding: 0;
+        margin-top: 5vh;
+    }
+    
+    .btn {
+        color: #313639;
+        margin: 0 1px;
+    }
+
+    input {
+        font-family: "Roboto", sans-serif;
+        color: white;
+    }
+
+    textarea {
+        font-family: "Roboto", sans-serif;
+        color: white;
+    }
+
+    #scroll {
+        width: 100%;
+        height: 90%;
+        overflow-x: hidden;
+        overflow-y: scroll;
+        position: relative;
+        display: flex;
+        justify-content: center;
+        align-items: flex-start;    
+
+        .content {
+            width: 95%;
+            min-height: 100%;
+            background-color: #FFFFFFF0;
+            color: black;
+            line-height: 150%;
+            @media screen and (min-width: 775px){
+                font-size: 1.5rem;
+            }
+
+            div {
+                margin: 20px;
+                display: flex;
+                flex-direction: column;
+            }
+        }
+    }
 }
 </style>
--- bl_frontend/src/views/Home.vue
+++ bl_frontend/src/views/Home.vue
@@ -11,3 +11,4 @@ export default {
   name: 'home'
 }
 </script>
+
