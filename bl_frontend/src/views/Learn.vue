c68298f460c05b6ab9a37f1203000d5cf638ff03
Mike McDonagh <mikemcdonagh@protonmail.com>
2019-11-20 11:32:42 -0500

    Migrated /learn from FrontEndMockup to /learn in Vue

---
+++ bl_frontend/src/views/Learn.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+    <div class="main" id="learn-main">
+        <div id="assigned-labs-container" class="container section row cyan darken-1">
+            <!-- v-for cards gotten via axios/ajax -->
+        </div>
+    </div>
+</template>
+
+<script>
+export default  {
+  name: 'learn'
+}
+</script>
+
+<style scoped lang="scss">
+#learn-main{
+    justify-content: center;
+    align-items: center;
+    #assigned-labs-container {
+        padding-left: 10px;
+        height: 90%;
+        overflow-x: hidden;
+        overflow-y: scroll;
+    }
+}
+</style>
