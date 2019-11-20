c3fcf69b051033c2413bf1d9a88543ab138ea2bc
Mike McDonagh <mikemcdonagh@protonmail.com>
2019-11-20 12:27:38 -0500

    Style changes and js restructure

--- bl_frontend/src/views/Learn.vue
+++ bl_frontend/src/views/Learn.vue
@@ -1,11 +1,14 @@
 <template lang="html">
     <div class="main" id="learn-main">
-        <div id="labs-container" class="container section row cyan darken-1">
+        <div id="labs-container" class="container section cyan darken-1">
             <div id="lab-header" class="row">
-                <h2 class="col s12">Labs</h2>
+                <h2 class="col s12">
+                    Labs
+                    <i class="material-icons">assignment</i>
+                </h2>
                 <div class="col s10 offset-s1 divider"></div>
             </div>
-            <div id="labs">
+            <div id="labs" class="row">
                 <!-- v-for cards gotten via axios/ajax -->
             </div>
         </div>
@@ -19,7 +22,14 @@ export default  {
     this.$store.commit('changeNav', 'cyan');
   },
   mounted() {
-    $('#labs').height($('#labs-container').height() - $('#lab-header').height());
+    this.sizeContent();
+    window.resize = this.sizeContent;
+  },
+  methods: {
+    sizeContent(){
+        console.log($('#labs-container').height(), $('#lab-header').height())
+        $('#labs').height($('#labs-container').height() - $('#lab-header').height());
+    }
   }
 }
 </script>
@@ -40,6 +50,11 @@ export default  {
             h2 {
                 text-align: center;
                 margin: 0;
+                i{
+                    position: relative;
+                    top: 7px;
+                    font-size: 1em;
+                }
             }
             div {
                 margin-bottom: 0;
