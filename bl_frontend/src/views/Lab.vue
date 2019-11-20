a253ecbdae682bf69d994466dcb53decb437a9a0
Mike McDonagh <mikemcdonagh@protonmail.com>
2019-11-20 13:16:09 -0500

    Migrated /lab from FrontEndMockup to /lab in Vue

--- bl_frontend/src/views/Lab.vue
+++ bl_frontend/src/views/Lab.vue
@@ -1,37 +1,75 @@
 <template lang="html">
-
-  <section class="lab">
-    <h1>lab Component</h1>
-  </section>
-
+  <div class="main row" id="lab-main">
+    <div id="parts-side-bar" class="cyan darken-1 col s3">
+      <h4 class="center">PARTS</h4>
+      <ul id="parts" class="collapsible">
+        <!-- v-for parts (possibly categorized)-->
+      </ul>
+      <div id="padding"></div>
+    </div>
+    <div id="workspace" class="col s9">
+      <div id="build-so-far"></div>
+    </div>
+    <div id="controls">
+      <button class="btn cyan lighten-3 waves-effect" id="save">SAVE</button>
+      <a href="/learn" class="btn cyan lighten-3 waves-effect" id="exit">EXIT</a>
+    </div>
+  </div>
 </template>
 
 <script lang="js">
 
-  export default  {
-    name: 'lab',
-    props: [],
-    mounted () {
+export default  {
+  name: 'lab',
+  created() {
+    this.$store.commit('changeNav', 'cyan');
+  },
+  mounted () {
 
-    },
-    data () {
-      return {
-
-      }
-    },
-    methods: {
-
-    },
-    computed: {
-
-    }
+  }
 }
 
 
 </script>
 
 <style scoped lang="scss">
-  .lab {
+#lab-main {
+  #parts-side-bar {
+    align-items: center;
+    overflow-x: hidden;
+    overflow-y: scroll;
+    padding: 0;
+    padding-bottom: 50px;
 
+    #parts {
+      margin: 0;
+      width: 100%;
+      box-shadow: none;
+    }
+    #padding {
+      height: 50px;
+    }
   }
+  #controls {
+    display: flex;
+    flex-direction: column;
+    position: fixed;
+    left: 10px;
+    bottom: 10px;
+    width: 25%;
+    z-index: 1;
+
+    .btn {
+      width: 95%;
+    }
+
+    @media screen and (min-width: 800px){
+      flex-direction: row;
+      .btn {
+          width: 45%;
+          font-size: 2rem;
+      }
+    }
+  }
+}
 </style>
