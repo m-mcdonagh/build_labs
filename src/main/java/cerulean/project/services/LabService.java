aa487722c7717bacfd47cb9ffc4f8c76fbac9280
Brian Yi <byi8220@gmail.com>
2019-11-24 18:08:08 -0500

    added step validation in lab building

--- src/main/java/cerulean/project/services/LabService.java
+++ src/main/java/cerulean/project/services/LabService.java
@@ -2,17 +2,13 @@ package cerulean.project.services;
 
 import cerulean.project.database.AccountRepository;
 import cerulean.project.database.LabRepository;
-import cerulean.project.models.Account;
-import cerulean.project.models.Lab;
-import cerulean.project.models.LabAssignment;
+import cerulean.project.models.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Component
 public class LabService {
@@ -39,5 +35,46 @@ public class LabService {
     }
 
     public void addNewLab(String username, Lab lab) {
+        labRepository.save(lab);
+    }
+
+
+    public List<Integer> validateLabLegality(Lab lab) {
+        List<Integer> invalidSteps = new ArrayList<>();
+        Map<Step, Set<Integer>> occupiedSlots = new HashMap<>();
+        for (Step step : lab.getSteps()) {
+
+            occupiedSlots.put(step, new HashSet<>()); // New step proposed
+            Integer parentIndex = step.getParentIndex();
+
+            // If this is the first step, there is nowhere for you to misplace something
+            if (step.getIndex() == 0) {
+                continue;
+            }
+            // If this step depends on an invalid parent or a nonexistent parent it's bad
+            if (invalidSteps.contains(parentIndex) || parentIndex >= lab.getSteps().size()) {
+                invalidSteps.add(step.getIndex());
+                continue;
+            }
+
+
+            Step parentStep = lab.getSteps().get(parentIndex);
+            Part parentPart = parentStep.getNewPart();
+
+            boolean stepOutOfBounds = step.getParentSlot() >= parentPart.getSlotPoints().size();
+            boolean slotAlreadyInUse = false;
+
+            if (occupiedSlots.containsKey(parentStep)) {
+                slotAlreadyInUse = occupiedSlots.get(parentStep).contains(step.getParentSlot());
+            }
+            boolean stepIsInvalid = stepOutOfBounds || slotAlreadyInUse;
+
+            if (stepIsInvalid) {
+                invalidSteps.add(step.getIndex());
+            } else {
+                occupiedSlots.get(parentStep).add(step.getParentSlot());
+            }
+        }
+        return invalidSteps;
     }
 }
