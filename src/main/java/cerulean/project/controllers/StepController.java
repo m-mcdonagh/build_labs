0bd4b8595007029b38bd5f9a158934d04b1b1402
ColinRoye <croye97@gmail.com>
2019-11-24 16:28:57 -0500

    changes to step controller

--- src/main/java/cerulean/project/controllers/StepController.java
+++ src/main/java/cerulean/project/controllers/StepController.java
@@ -1,9 +1,34 @@
 package cerulean.project.controllers;
 
-public class StepController {
+
+import cerulean.project.models.Account;
+import cerulean.project.services.AccountService;
+import cerulean.project.services.StepControllerService;
+import com.google.gson.Gson;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
 
 //    /steps/step/{lab_Id}/{index}		GET		    Get Instruction
 //    /steps/step						POST		Add Instruction
-    
+
+
+@RestController
+@RequestMapping(value ="/steps")
+public class StepController {
+    private StepControllerService stepService;
+    private AccountService accountService;
+    private Gson gson = new Gson();
+
+
+    @RequestMapping(value ="/", method = RequestMethod.GET)
+    public void listParts(@RequestParam String lab_id, @RequestParam String index, HttpServletResponse httpResponse) {
+
+
+    }
+
 
 }
