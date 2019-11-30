d2df0193904f65795bfc0cc2e48a6b04347c7a94
Mike McDonagh <mikemcdonagh@protonmail.com>
2019-11-30 17:00:09 -0500

    Added a lab info modal

--- bl_frontend/src/components/Create/LabList.vue
+++ bl_frontend/src/components/Create/LabList.vue
@@ -21,7 +21,7 @@
                     <i class="material-icons left">publish</i>Publish
                 </a>
             </li>
-            <li v-if="ispublished">
+            <li v-if="ispublished" class="modal-trigger" v-bind:href="'#lab-modal-' + id">
                 <a>
                     <i class="material-icons">info</i>Info
                 </a>
@@ -33,6 +33,31 @@
             </li>
         </ul>
         <span>{{ name }}</span>
+        <div v-if="ispublished" v-bind:id="'lab-modal-' + id" class="modal">
+            <div class="modal-content indigo lighten-4">
+                <h4><i class="material-icons">assignment</i>{{ name }}</h4>
+                <div class="collection">
+                    <div v-for="i in info" v-bind:key="i.user_id" class="collection-item info">
+                        {{i.username}}
+                        <span v-if="!i.complete" 
+                              class="tooltipped"
+                              data-position="left"
+                              data-tooltip="INCOMPLETE">
+                            &#9744;
+                        </span>
+                        <span v-if="i.complete">&#9744;</span>
+                        <img v-if="i.complete" 
+                             class="check tooltipped"
+                             data-position="left"
+                             data-tooltip="COMPLETE"
+                             src="../../assets/img/check.png"/>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer indigo lighten-3">
+                <button class="btn modal-close waves-effect waves-light indigo accent-4">Close</button>
+            </div>
+        </div>
     </li>
 </template>
 
@@ -42,11 +67,18 @@ export default {
     props: ['name', 'ispublished', 'id'],
     data() {
         return {
-            link: '/labbuilder?id=' + this.id
+            link: '/labbuilder?id=' + this.id,
+            // TODO: set up axios for this.info
+            info: [
+                {user_id:0, username:'not complete', complete:false}, 
+                {user_id:1, username:'complete', complete:true}
+            ]
         }
     },
     mounted(){
         M.Dropdown.init($(this.$el).find('.dropdown-trigger').get(0), {constrainWidth: false, coverTrigger: false});
+        M.Modal.init($(this.$el).find('.modal').get(0), {});
+        M.Tooltip.init($(this.$el).find('.tooltipped').get(0), {position: 'left'});
     },
     methods: {
         copy() {
@@ -72,6 +104,23 @@ export default {
 
     span {
         margin: 5px;
+    }
+    .btn{
+        color: white !important;
+    }
+    .info{ 
+        position: relative;
+        span{
+            position: absolute;
+            right: 10px;
+            cursor: default;
+        }
+        img{
+            position: absolute;
+            height: 80%;
+            top: 0;
+            right: 5px;
+        }
     }
 }
 </style>
