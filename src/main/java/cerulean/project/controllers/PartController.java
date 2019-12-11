331dc94fd2754abc3eeee4e70f72f1937f753108
Khurram Malik <khurrammalik97@gmail.com>
2019-12-11 00:19:24 -0500

    Editing an existing part that is not published now works

--- src/main/java/cerulean/project/controllers/PartController.java
+++ src/main/java/cerulean/project/controllers/PartController.java
@@ -64,6 +64,54 @@ public class PartController {
         return gson.toJson(part);
     }
 
+    @RequestMapping(value = "/part/updatepart", method = RequestMethod.POST)
+    public String updatePart(@RequestBody String partJson, @RequestParam String username) {
+        JsonObject jsonObject = gson.fromJson(partJson, JsonObject.class);
+
+        List<List<Double>> slotPoints = new ArrayList<>();
+        List<Double> connectorPoint = new ArrayList<>();
+
+        JsonArray jarray = jsonObject.getAsJsonArray("slotPoints");
+        String connectorPoint_String = jsonObject.get("connectorPoint").toString();
+        jsonObject.remove("connectorPoint");
+
+        String part_id = jsonObject.get("_id").toString();
+        part_id = part_id.substring(1,part_id.length()-1); //get rid of " "
+
+        String[] xy_s = connectorPoint_String.split(",");
+
+        Double conn_x = Double.parseDouble(xy_s[0].substring(xy_s[0].indexOf(":") + 1)) / 100;
+        Double conn_y = Double.parseDouble(xy_s[1].substring(xy_s[1].indexOf(":") + 1, xy_s[1].indexOf("}"))) / 100;
+
+        connectorPoint.addAll(Arrays.asList(conn_x, conn_y));
+
+        for (int i = 0; i < jarray.size(); i++) {
+            JsonObject obj = jsonObject.getAsJsonArray("slotPoints").get(i).getAsJsonObject();
+            //obj.remove("id");
+            Double x = Double.parseDouble(obj.remove("x").toString()) / 100;
+            Double y = Double.parseDouble(obj.remove("y").toString()) / 100;
+            //String  x = obj.remove("x").toString();
+            //String  y = obj.remove("y").toString();
+            slotPoints.add(Arrays.asList(x, y));
+        }
+
+        jsonObject.remove("slotPoints");
+        jsonObject.addProperty("ispublished", false);
+
+        //System.out.println(jsonObject);
+
+        Part part = gson.fromJson(jsonObject, Part.class);
+        part.setSlotPoints(slotPoints);
+        part.setConnectorPoint(connectorPoint);
+        partService.updatePart(part_id,part);
+
+
+
+        return "temp";
+    }
+
+
+
     @RequestMapping(value = "/part", method = RequestMethod.POST)
     public String addPart(@RequestBody String partJson, @RequestParam String username) {
         System.out.println("Add part controller");
