6191aeb0633b15e4a55b784a6bf639c1ed4be82e
brian yi <byi8220@gmail.com>
2019-12-11 01:21:28 -0500

    form based login post

--- bl_frontend/src/views/Login.vue
+++ bl_frontend/src/views/Login.vue
@@ -1,7 +1,7 @@
 <template lang="html">
 <div class="main" id="login-main">
   <div id="login" class="container light-blue darken-1">
-    <form id="login-form"  name="loginForm" class="section" action="/api/login">
+    <form id="login-form"  name="loginForm" class="section" action="/api/login" method="post">
       <h1 class="center">Login</h1>
       <div class="row">
         <div class="input-field col s12 m10 offset-m1">
