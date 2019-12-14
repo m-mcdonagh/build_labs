c3a02cc261fd5506ea6502dc0ce653ae9cbf1d84
ColinRoye <croye97@gmail.com>
2019-12-14 14:56:04 -0500

    reworking add media

--- src/main/java/cerulean/project/controllers/PartController.java
+++ src/main/java/cerulean/project/controllers/PartController.java
@@ -19,7 +19,15 @@ import java.util.*;
 
 import javax.servlet.http.HttpServletResponse;
 import com.google.gson.Gson;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import java.security.MessageDigest;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 @RestController
 @RequestMapping(value ="/api/parts")
 public class PartController {
@@ -207,6 +215,26 @@ public class PartController {
         }
         System.out.println(partId);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+    private static String UPLOADED_FOLDER = "/Users/Colin/Desktop/";
+
+    @PostMapping("/addMedia")
+    public String singleFileUpload(@RequestParam("file") MultipartFile file,
+                                   RedirectAttributes redirectAttributes) {
+        if (file.isEmpty()) {
+            redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
+            return "redirect:uploadStatus";
+        }
+        try {
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+            Files.write(path, bytes);
+            redirectAttributes.addFlashAttribute("message",
+                    "You successfully uploaded '" + file.getOriginalFilename() + "'");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "redirect:/uploadStatus";
     }
 
 }
